4340b3dbb410decf11ae2592b9d19fcf3f389b93
HienLT52 <thanhhienle95@gmail.com>
2021-07-31 23:24:14 +0700

    Dùng useState() qua ví dụ đơn giản (2020)

--- src/App.js
+++ src/App.js
@@ -1,29 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import Form from './components/form-register/form-register';
-import { useState } from 'react';
+import ColorBox from './components/ColorBox/ColorBox';
 
 function App() {
   return (
     <div className="App">
+      <h1>Welcome to React Hooks!</h1>
       <Form></Form>
+      <ColorBox></ColorBox>
     </div>
   );
 }
-
-// function App() {
-//   const [count, setCount] = useState(0);
-//   console.log(count);
-// // Ta có useState là một Hook. Hàm useState nhận tham số initial state
-// // sau đó sẽ trả về một mảng 2 phần tử:
-// // phần tử đầu tiên là state hiện tại
-// // thứ 2 là hàm để update state - (hàm thứ hai này giống với setState khi chúng sử dụng dạng Class)
-
-//   return (
-//     <div>
-//       <p>You clicked {count} times</p>
-//       <button onClick={() => setCount(count + 1)}>Click me</button>
-//     </div>
-//   )
-// }
 export default App;
